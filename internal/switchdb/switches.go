d1e8fdc29901f46f5b329b5a037eafdfd95b272a
FrizakN1 <frizak2017@mail.ru>
2026-04-13 14:02:16 +0500

    добавил поддержку моделей eltex 1124 и 2124

--- internal/switchdb/switches.go
+++ internal/switchdb/switches.go
@@ -2,6 +2,23 @@ package switchdb
 
 import "snmp/internal/domain"
 
+var eltexMES2324OID = domain.SwitchOID{
+	Firmware:                  "1.3.6.1.4.1.89.2.16.1.1.4.1",
+	SystemName:                "1.3.6.1.2.1.1.5.0",
+	SysLocation:               "1.3.6.1.2.1.1.6.0",
+	SN:                        "1.3.6.1.4.1.89.53.14.1.5.1",
+	SaveConfig:                "1.3.6.1.4.1.89.87.2.1",
+	PortDesc:                  "1.3.6.1.2.1.31.1.1.1.18",
+	BatteryCharge:             "1.3.6.1.4.1.35265.1.23.11.1.1.3",
+	BatteryStatus:             "1.3.6.1.4.1.35265.1.23.11.1.1.2",
+	PortMode:                  "1.3.6.1.4.1.89.48.22.1.1",
+	CPUUtilizationFiveSeconds: "1.3.6.1.4.1.89.1.7.0",
+	CPUUtilizationOneMinutes:  "1.3.6.1.4.1.89.1.8.0",
+	CPUUtilizationFiveMinutes: "1.3.6.1.4.1.89.1.9.0",
+	CPUTemperature:            "1.3.6.1.4.1.89.53.15.1.10.1",
+	TransceiverInfo:           "1.3.6.1.4.1.89.90.1.2.1.3",
+}
+
 var Switches = map[string]domain.SwitchOID{
 	"DES-1210-28": {
 		Firmware:     "1.3.6.1.4.1.171.10.75.5.2.1.3.0",
@@ -161,36 +178,10 @@ var Switches = map[string]domain.SwitchOID{
 		VlanUntagged: "1.3.6.1.2.1.17.7.1.4.3.1.4",
 	},
 
-	"MES2324FB": { //https://eltexcm.ru/assets/docs/site/MES_configuration_and_monitoring_via_SNMP_4_0_16_5.pdf
-		Firmware:                  "1.3.6.1.4.1.89.2.16.1.1.4.1",
-		SystemName:                "1.3.6.1.2.1.1.5.0",
-		SysLocation:               "1.3.6.1.2.1.1.6.0",
-		SN:                        "1.3.6.1.4.1.89.53.14.1.5.1",
-		SaveConfig:                "1.3.6.1.4.1.89.87.2.1",
-		PortDesc:                  "1.3.6.1.2.1.31.1.1.1.18",
-		BatteryCharge:             "1.3.6.1.4.1.35265.1.23.11.1.1.3",
-		BatteryStatus:             "1.3.6.1.4.1.35265.1.23.11.1.1.2",
-		PortMode:                  "1.3.6.1.4.1.89.48.22.1.1",
-		CPUUtilizationFiveSeconds: "1.3.6.1.4.1.89.1.7.0",
-		CPUUtilizationOneMinutes:  "1.3.6.1.4.1.89.1.8.0",
-		CPUUtilizationFiveMinutes: "1.3.6.1.4.1.89.1.9.0",
-		CPUTemperature:            "1.3.6.1.4.1.89.53.15.1.10.1",
-		TransceiverInfo:           "1.3.6.1.4.1.89.90.1.2.1.3",
-	},
-	"MES2300-24": { //https://eltex-msk.ru/docsnew/MES2428B/mes-configuration-and-monitoring-via-snmp-10363.pdf?ysclid=mm1ryun5cm863179458
-		Firmware:                  "1.3.6.1.4.1.89.2.16.1.1.4.1",
-		SystemName:                "1.3.6.1.2.1.1.5.0",
-		SysLocation:               "1.3.6.1.2.1.1.6.0",
-		SN:                        "1.3.6.1.4.1.89.53.14.1.5.1",
-		SaveConfig:                "1.3.6.1.4.1.89.87.2.1",
-		PortDesc:                  "1.3.6.1.2.1.31.1.1.1.18",
-		PortMode:                  "1.3.6.1.4.1.89.48.22.1.1",
-		CPUUtilizationFiveSeconds: "1.3.6.1.4.1.89.1.7.0",
-		CPUUtilizationOneMinutes:  "1.3.6.1.4.1.89.1.8.0",
-		CPUUtilizationFiveMinutes: "1.3.6.1.4.1.89.1.9.0",
-		CPUTemperature:            "1.3.6.1.4.1.89.53.15.1.10.1",
-		TransceiverInfo:           "1.3.6.1.4.1.89.90.1.2.1.3",
-	},
+	"MES2324FB":  eltexMES2324OID, //https://eltexcm.ru/assets/docs/site/MES_configuration_and_monitoring_via_SNMP_4_0_16_5.pdf
+	"MES2300-24": eltexMES2324OID,
+	"MES1124MB":  eltexMES2324OID,
+	"MES2124MB":  eltexMES2324OID,
 	"MES2428B": { //https://eltex-msk.ru/docsnew/MES2428B/mes-configuration-and-monitoring-via-snmp-10363.pdf?ysclid=mm1ryun5cm863179458
 		Firmware:                  "1.3.6.1.4.1.35265.1.139.18.1.1.3.1.1.4.1.1",
 		SystemName:                "1.3.6.1.2.1.1.5.0",
@@ -209,11 +200,4 @@ var Switches = map[string]domain.SwitchOID{
 		CPUUtilizationFiveMinutes: "1.3.6.1.4.1.35265.1.139.6.1.1.2.3.0",
 		CPUTemperature:            "1.3.6.1.4.1.2076.81.1.66.0",
 	},
-
-	//snmpset -v2c -c <community> <IP address> \
-	//1.3.6.1.4.1.89.87.2.1.3.1 i {local(1)} \
-	//1.3.6.1.4.1.89.87.2.1.7.1 i {runningConfig(2)} \
-	//1.3.6.1.4.1.89.87.2.1.8.1 i {local(1)} \
-	//1.3.6.1.4.1.89.87.2.1.12.1 i {startupConfig (3)} \
-	//1.3.6.1.4.1.89.87.2.1.17.1 i {createAndGo (4)}
 }
